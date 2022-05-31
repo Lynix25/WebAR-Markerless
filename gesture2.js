efb235e31b22b444620581ec143912d46c43927e
Lynix25 <pauleric22524@gmail.com>
2022-05-31 15:30:03 +0700

    3:29 5/31/2022

--- gesture2.js
+++ gesture2.js
@@ -1,79 +1,18 @@
-let _
-let id = setInterval(() => {
-  _ = document.getElementById("console");
-  if (_) {
-    clearInterval(id);
-  }
-}, 50)
-
-function track(message, flag = true) {
-  _.innerHTML = message;
-  if (flag) console.log(message);
-}
-
-function gestureHandler(eventName, event, object) {
-  let enabled = true,
-    rotationFactor = 5,
-    minScale = 0.3,
-    maxScale = 8;
-
-  init();
-
-  function init() {
-    object.isVisible = true;
-    object.scaleFactor = 1;
-  }
-
-  track(eventName);
-  if (eventName == "onefingermove") {
-    handleRotation();
-  }
-  else if (eventName == "twofingermove") {
-    handleScale();
-    track("Scale Implement");
-  }
-
-  function handleRotation() {
-    if (object.isVisible) {
-      let temp_x = event.positionChange.x
-      let temp_y = event.positionChange.y
-      object.rotateY(temp_x * rotationFactor)
-      object.rotateX(temp_y * rotationFactor)
-    }
-  }
-
-  function handleScale() {
-    // if (object.isVisible) {
-      scaleFactor *= 1 + event.spreadChange / event.startSpread;
-      track("Scale Handle");
-      scaleFactor = Math.min(
-        Math.max(scaleFactor, minScale), maxScale);
-
-      // object.scale.x = scaleFactor * initialScale.x;
-      // object.scale.y = scaleFactor * initialScale.y;
-      // object.scale.z = scaleFactor * initialScale.z;
-      object.scale.set(scaleFactor * initialScale.x, scaleFactor * initialScale.y, scaleFactor * initialScale.z);
-  // }
-  }
-}
-
-// Component that detects and emits events for touch gestures
-
 export function gestureDetector(object, target) {
-  init()
-  function init() {
-    console.log("gesture detector")
+  // ===== Initial States START =====
+  object.internalState = {
+    previousState: null
+  };
 
-    object.internalState = {
-      previousState: null
-    };
+  object.addEventListener("touchstart", emitGestureEvent);
+  object.addEventListener("touchend", emitGestureEvent);
+  object.addEventListener("touchmove", emitGestureEvent);
 
-    object.addEventListener("touchstart", emitGestureEvent);
+  target.isVisible = true;
+  target.scaleFactor = 1;
+  target.initialScale = target.scale.clone();
 
-    object.addEventListener("touchend", emitGestureEvent);
-
-    object.addEventListener("touchmove", emitGestureEvent);
-  }
+  // ===== Initial States END =====
 
   function emitGestureEvent(event) {
     const currentState = getTouchState(event);
@@ -92,9 +31,6 @@ export function gestureDetector(object, target) {
     if (gestureEnded) {
       const eventName = getEventPrefix(previousState.touchCount) + "fingerend";
 
-      // this.el.emit(eventName, previousState); IMPORTANT
-      // gestureHandler(eventName, previousState, target);
-
       object.internalState.previousState = null;
     }
 
@@ -106,9 +42,6 @@ export function gestureDetector(object, target) {
       currentState.startSpread = currentState.spread;
 
       const eventName = getEventPrefix(currentState.touchCount) + "fingerstart";
-
-      // this.el.emit(eventName, currentState); IMPORTANT
-      // gestureHandler(eventName, currentState, target);
 
       object.internalState.previousState = currentState;
     }
@@ -146,7 +79,6 @@ export function gestureDetector(object, target) {
     }
 
     // Convert event.touches to an array so we can use reduce
-
     const touchList = [];
 
     for (let i = 0; i < event.touches.length; i++) {
@@ -204,3 +136,36 @@ export function gestureDetector(object, target) {
     return numberNames[Math.min(touchCount, 4) - 1];
   }
 };
+
+function gestureHandler(eventName, event, object) {
+  let enabled = true,
+    rotationFactor = 5,
+    minScale = 0.3,
+    maxScale = 8;
+
+  if (eventName == "onefingermove") {
+    handleRotation();
+  }
+  else if (eventName == "twofingermove") {
+    handleScale();
+  }
+
+  function handleRotation() {
+    console.log(event)
+    if (object.isVisible) {
+      let temp_x = event.positionChange.x;
+      let temp_y = event.positionChange.y;
+      object.rotateY(temp_x * rotationFactor);
+      object.rotateX(temp_y * rotationFactor);
+    }
+  }
+
+  function handleScale() {
+    if (object.isVisible) {
+      object.scaleFactor *= 1 + event.spreadChange / event.startSpread;
+      object.scaleFactor = Math.min(Math.max(object.scaleFactor, minScale), maxScale);
+      object.scale.set(object.scaleFactor * object.initialScale.x, object.scaleFactor * object.initialScale.y, object.scaleFactor * object.initialScale.z);
+      _.innerHTML = `SCALE ${object.scale.x} ${object.scale.y} ${object.scale.z}`;
+    }
+  }
+}
